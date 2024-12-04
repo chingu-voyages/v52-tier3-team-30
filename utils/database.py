cd0275a3b1e3105fb55b3321fc3e907d4f423035
hua-lun <chuahualun@gmail.com>
2024-12-04 20:34:49 +0800

    Add helper functions for firestore

---
+++ utils/database.py
@@ -0,0 +1,37 @@
+import datetime
+
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim(user_agent="v52-tier3-team-30")
+
+DOC_NAME = '2024'
+
+def get_geopoint(address):
+    location = geolocator.geocode(address)
+    return [location.latitude, location.longitude]
+
+def convert_timestamp(timeslot, preferred_date):
+    datetime_str = preferred_date + ' ' + timeslot
+    datetime_obj = datetime.datetime.strptime(datetime_str, '%Y-%m-%d %H:%M')
+
+    return datetime_obj
+
+
+def create_resident(db, name, email, phone, address, timeslot, preferred_date):
+
+    timestamp = convert_timestamp(timeslot, preferred_date)
+    geopoint = get_geopoint(address)
+
+    doc_ref = db.collection('residents')
+    update_time, resident_ref = doc_ref.add({
+        'name': name,
+        'email': email,
+        'phone': phone,
+        'address': geopoint,
+        'timeslot': timestamp,
+    })
+
+    print('Resident created')
+
+
+
---
+++ utils/firebase_config.py
@@ -0,0 +1,8 @@
+firebaseConfig = {
+  "apiKey": "AIzaSyBNmcyBXqUO8ZFFG_G1hsu8NEV11a5e2Jc",
+  "authDomain": "v52-tier3-team-30.firebaseapp.com",
+  "projectId": "v52-tier3-team-30",
+  "storageBucket": "v52-tier3-team-30.firebasestorage.app",
+  "messagingSenderId": "443358892905",
+  "appId": "1:443358892905:web:d3cf51373e3300290423f8"
+}
