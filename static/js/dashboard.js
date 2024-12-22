bfe6b507972408ca901c795d1767cfc5c67c509b
hua-lun <chuahualun@gmail.com>
2024-12-22 17:39:39 +0800

    Add highlight button for current view

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -16,21 +16,31 @@ map_button.addEventListener('click', onClickMapButton)
 function onClickListButton() {
     map_view.hidden = true
     list_view.hidden = false
+    map_button.style.borderColor = '#9a88ce'
+    list_button.style.borderColor = '#be8a3b'
+
 }
 
 function onClickMapButton() {
     map_view.hidden = false
     list_view.hidden = true
+    map_button.style.borderColor = '#be8a3b'
+    list_button.style.borderColor = '#ce8899'
 }
 
 function onClickAllListButton() {
     today_table_view.hidden = true
     all_table_view.hidden = false
+    all_list_view.style.borderColor = '#8e5f6a'
+    today_list_view.style.borderColor = '#ec5e94'
 }
 
 function onClickTodayListButton() {
     today_table_view.hidden = false
     all_table_view.hidden = true
+    all_list_view.style.borderColor = '#f1ad47'
+    today_list_view.style.borderColor = '#8e5f6a'
+
 }
 
 $(document).ready( function () {
